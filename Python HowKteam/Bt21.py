d5184a90c2861626f63cd3b089849aa7e48a176c
Hoang Vy <da.hoangvy@gmail.com>
2023-06-27 23:55:53 +0700

    Bt22

--- Python HowKteam/Bt21.py
+++ Python HowKteam/Bt21.py
@@ -1,13 +1,16 @@
 # Viết chương trình nhập vào hệ số a,b,c của phương trình bậc 2 và giải phương trình đó.
+# Có xử lí ngoại lệ
 import math
 
 a,b,c = input('Nhap vao he so a, b, c: ').split()
 x1 = 0
 x2 = 0
-a = float(a)
-b = float(b)
-c=float(c)
-if a == 0:
+
+try:
+    a = float(a)
+    b = float(b)
+    c=float(c)
+    if a == 0:
         if b==0:
             if c == 0:
                 print("Phuong trinh vo so nghiem.")
@@ -17,7 +20,7 @@ if a == 0:
             x1 = (-b)/(2*a)
             x2 = x1
             print(f'Phuong trinh co nghiem kep gia tri la: {x1}')
-else:
+    else:
         delta = b**2 - (4*a*c)
         if delta > 0:
             x1 = (-b + math.sqrt(delta))/(2*a)
@@ -28,3 +31,5 @@ else:
             print(f'Phuong trinh co nghiem kep x1 = x2 = {x1}')
         else:
             print('Phuong trinh vo nghiem')
+except:
+    print('Nhap vao khong hop le')
