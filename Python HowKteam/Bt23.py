25720b5200a44d79c3d10d164b73a2be40a9ed43
dahoangvy <da.hoangvy@gmail.com>
2023-07-06 23:19:10 +0700

    Update

--- Python HowKteam/Bt23.py
+++ Python HowKteam/Bt23.py
@@ -1,12 +1,17 @@
 # Viết chương trình nhập vào từ file input ba số a, b, c. 
 # Nếu a, b, c là ba cạnh của một tam giác thì kiểm tra và xuất ra file output thông báo loại của tam giác (Có xử lý ngoại lệ đầu vào).
 
-with open('Python HowKteam\Bt23.inp', "r") as inPut:
-    baCanh = inPut.readline().strip().split()
+
 try:
+    with open('Python HowKteam\Bt23.inp', "r") as inPut:
+        baCanh = inPut.readline().strip().split()
     a,b,c = map(float, baCanh)
+
     loai = None
-    if(a+b > c and a+c > b  and b+c> a):
+
+    if a<=0 or b<=0 or c<=0:
+       thongBao = "Cac canh cua tam giac phai lon hon 0!"
+    elif(a+b > c and a+c > b  and b+c> a):
         if (a*a == b*b + c*c or b*b == a*a + c*c or (c*c == a*a + b*b)):
             loai = 'vuong'
         elif (a == b and a == c):
@@ -17,11 +22,14 @@ try:
             loai = 'tu'
         else:
             loai = 'nhon'
-        with open('Python HowKteam\Bt23.oup', "w") as outPut:
-            outPut.write(f'{a}, {b}, {c} la ba canh cua mot tam giac {loai}')
+        thongBao = (f'{a}, {b}, {c} la ba canh cua mot tam giac {loai}')
     else:
-        with open('Python HowKteam\Bt23.oup', "w") as outPut:
-            outPut.write(f'{a}, {b}, {c} không la ba canh cua mot tam giac')
+        thongBao = (f'{a}, {b}, {c} khong la ba canh cua mot tam giac {loai}')
+    
+except FileNotFoundError:
+    thongBao = 'Khong tim thay file input!'
 except:
-    with open('Python HowKteam\Bt23.oup', "w") as outPut:
-            outPut.write('Nhap vao khong hop le')
+    thongBao= 'Dinh dang dau vao khong hop le'
+
+with open('Python HowKteam\Bt23.oup', "w") as outPut:
+            outPut.write(thongBao)
