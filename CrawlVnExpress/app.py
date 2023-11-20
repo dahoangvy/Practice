0ad7bb7bfa81b78340a2a1aa7b74194196ec46f6
dahoangvy <da.hoangvy@gmail.com>
2023-11-20 22:45:49 +0700

    fix crawl func

--- CrawlVnExpress/app.py
+++ CrawlVnExpress/app.py
@@ -7,7 +7,7 @@ token = "6961893297:AAG0KpeoBVQJmyByBOmhg4QpP8Zvv3j8xXc"
 chat_id = -4097991853
 
 
-def get_data(url):
+def get_data(url,token,chat_id):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     tags_h2 = soup.find_all("h2", class_="title-news")
@@ -21,25 +21,21 @@ def get_data(url):
         for tag_a in tags_a:
             url_post = tag_a['href']
             url_list.append(url_post)
-
+        send_message(token,chat_id,title,url_post)
     dict_data = {"Title": title_list,
                  "Url": url_list}
     return dict_data
 
 
-def send_message(token, chat_id, func):
+def send_message(token, chat_id, title, url):
 
     try:
-        dict_data = func()
-        for title, url in dict_data.items():
-
-            # Gửi vào group Telegram theo từng cặp Title + URL của bài báo
-            urlPost = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={title}\n{url}"
-            requests.post(urlPost)
+        # Gửi vào group Telegram theo từng cặp Title + URL của bài báo
+        urlPost = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={title}\n{url}"
+        requests.post(urlPost)
 
     except Exception as e:
         urlPost = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={e}"
         requests.post(urlPost)
 
-
-send_message(token, chat_id, (get_data(url)))
+get_data(url,token,chat_id)
